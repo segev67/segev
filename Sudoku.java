486ef37e2f51ce1422b942a12f9e97fb2d104130
segev67 <73983009+segev67@users.noreply.github.com>
2021-12-11 21:24:32 +0200

    Add files via upload

--- Sudoku.java
+++ Sudoku.java
@@ -1,60 +1,79 @@
-public class Sudoku
-{
+public class Sudoku {
+
     private static final int SIZE = 3;
     private Square3x3[][] array;
 
-    public Sudoku()
-    {      
+    public Sudoku() {
         array = new Square3x3[SIZE][SIZE];
 
-        for (int i=0; i < array.length; i++){
-            for (int j=0; j < array[0].length; j++){
-                array[i][j] = new Square3x3(); 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = new Square3x3();
             }
         }
-
     }
 
-    public Sudoku(Square3x3[][] square3x3array)
-    {
+    public Sudoku(Square3x3[][] square3x3array) {
         this.array = new Square3x3[SIZE][SIZE];
-        for (int i=0; i < array.length; i++){
-            for (int j=0; j < array[0].length; j++){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 this.array[i][j] = new Square3x3(square3x3array[i][j]);
             }
         }
     }
-    
-    public boolean isValid()
-    {
-        for (int outsideRow = 0; outsideRow < SIZE; outsideRow++){
-            for (int insideRow = 0; insideRow < SIZE; insideRow++){
-                //if there is an valid row the whole sudoku is invalid
-                if (this.isValidRow(outsideRow, insideRow) == false){
+
+    public boolean isValid() {
+        return checkAllRows() && checkAllCols() && checkAllSquares();
+    }
+
+    private boolean checkAllSquares() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (!array[i][j].allThere()) {
                     return false;
                 }
             }
         }
-        
         return true;
     }
-    
-    private boolean isValidRow(int outsideRow, int insideRow)
-    {
-        //initilize false values array
-        boolean[] val = new boolean[10];
-        for (int j = 0; j < SIZE; j++){
-            array[outsideRow][j].whosThereRow(insideRow, val);
+
+    private boolean checkAllRows() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                boolean[] values = new boolean[10];
+                for (int x = 0; x < SIZE; x++) {
+                    this.array[j][x].whosThereRow(i, values);
+                }
+                // TODO :: Check boolean array if all true
+                if (!checkValues(values)) {
+                    return false;
+                }
+            }
         }
-        return isAllTrue(val);
+        return true;
     }
-    
-    
-    private static boolean isAllTrue(boolean[] val)
-    {
-        for (int i = 0; i < val.length; i++){
-            if (val[i] == false)
+
+    private boolean checkAllCols() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                boolean[] values = new boolean[10];
+                for (int x = 0; x < SIZE; x++) {
+                    this.array[x][j].whosThereCol(i, values);
+                }
+                // TODO :: Check boolean array if all true
+                if (!checkValues(values)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkValues(boolean[] values) {
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] == false) {
                 return false;
+            }
         }
         return true;
     }
