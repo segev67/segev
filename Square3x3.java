d3cad55989598d3f899d184143feb680c2811e31
segev67 <73983009+segev67@users.noreply.github.com>
2021-12-11 21:24:07 +0200

    Add files via upload

--- Square3x3.java
+++ Square3x3.java
@@ -1,71 +1,63 @@
-public class Square3x3
-{
+public class Square3x3 {
+
     private static final int SIZE = 3;
     private int[][] array;
 
-    public Square3x3()
-    {
+    public Square3x3() {
         array = new int[SIZE][SIZE];
 
-        for (int i=0; i < array.length; i++){
-            for (int j=0; j < array[0].length; j++){
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
                 array[i][j] = -1;
             }
         }
     }
 
-    public Square3x3(int[][] array)
-    {
+    public Square3x3(int[][] array) {
         this.array = new int[SIZE][SIZE];
 
-        for (int i=0; i < this.array.length; i++){
-            for (int j=0; j < this.array[0].length; j++){
-                if (i < array.length && j < array.length){
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
+                if (i < array.length && j < array[i].length) {
                     this.array[i][j] = array[i][j];
-                }
-                else {
+                } else {
                     this.array[i][j] = -1;
                 }
             }
         }
     }
 
-    public Square3x3(Square3x3 other)
-    {
+    public Square3x3(Square3x3 other) {
         this.array = new int[SIZE][SIZE];
 
-        for (int i = 0; i < this.array.length; i++){
-            for (int j=0; j < this.array[0].length; j++){
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
                 this.array[i][j] = other.array[i][j];
             }
         }
     }
 
-    public int getCell(int row, int col)
-    {
-        if (row >= 0 && col >=0 && row < SIZE && col < SIZE){
+    public int getCell(int row, int col) {
+        if (row >= 0 && col >= 0 && row < SIZE && col < SIZE) {
             return array[row][col];
         }
-        //row or col out of the bound  
+        //row or col out of the bound
         return -1;
     }
 
-    public void setXY(int row, int col, int value)
-    {
-        if (row >= 0 && col >= 0 && row < SIZE && col < SIZE){
+    public void setXY(int row, int col, int value) {
+        if (row >= 0 && col >= 0 && row < SIZE && col < SIZE) {
             array[row][col] = value;
         }
     }
 
-    public String toString()
-    {
+    public String toString() {
         String s = "";
-        for (int i=0; i < this.array.length; i++){
-            for (int j=0; j < this.array[0].length; j++){
-                if (j == (SIZE-1)){
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
+                if (j == (SIZE - 1)) {
                     s += array[i][j] + "\n";
-                }
-                else {    
+                } else {
                     s += array[i][j] + "\t";
                 }
             }
@@ -75,38 +67,43 @@ public class Square3x3
 
     //Q2
 
-    public boolean allThere()
-    {
-        for(int k = 1; k < 10; k++){
-            if (this.inSquare3x3(k) == false){
+    public boolean allThere() {
+        for (int k = 1; k < 10; k++) {
+            if (this.inSquare3x3(k) == false) {
                 //k is not in the array
                 return false;
             }
-        }//all the values between 1 and 9 are in the array
+        } //all the values between 1 and 9 are in the array
         return true;
     }
 
-    private boolean inSquare3x3(int x)
-    {
-        for (int i = 0; i < this.array.length; i++){
-            for (int j = 0; j < this.array[0].length; j++){
+    private boolean inSquare3x3(int x) {
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
                 //check if k is in the array
-                if (x == array[i][j])
-                    return true;
+                if (x == array[i][j]) return true;
             }
         }
         //x is not in the array
         return false;
     }
 
-    public void whosThereRow(int row, boolean[] values)
-    {
-        if (row < 0 || row >= SIZE || values.length != 10)
-            return;
+    public void whosThereRow(int row, boolean[] values) {
+        if (row < 0 || row >= SIZE || values.length != 10) return;
 
-        for (int j = 0; j < SIZE; j++){
-            if (array[row][j] >= 1 && array[row][j] <= 9){
+        for (int j = 0; j < SIZE; j++) {
+            if (array[row][j] >= 1 && array[row][j] <= 9) {
                 values[array[row][j]] = true;
+            }
+        }
+    }
+
+    public void whosThereCol(int col, boolean[] values) {
+        if (col < 0 || col >= SIZE || values.length != 10) return;
+
+        for (int j = 0; j < SIZE; j++) {
+            if (array[j][col] >= 1 && array[j][col] <= 9) {
+                values[array[j][col]] = true;
             }
         }
     }
